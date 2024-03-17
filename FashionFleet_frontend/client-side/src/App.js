dd2ddb63f4474ebddc2c5c208ec87bcd0ba241a1
S.M.D.Sanka Siriwardhana <siriwardhanad.sanka@gmail.com>
2024-03-17 12:35:36 +0530

    commit 2

---
+++ FashionFleet_frontend/client-side/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h1>This is Client Side</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
